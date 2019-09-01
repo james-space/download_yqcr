283c707d6ef89386caceaf856c22c66a9be227ed
james.cheng <cheng880204@126.com>
2019-09-01 23:25:14 +0800

    [note]download yqcr ver.2
    [description]
    download more pngs.

--- dl_ct.py
+++ dl_ct.py
@@ -6,7 +6,7 @@ try:
 except Exception as exc:
     print('there was a problem:%s'%(exc))
     
-print(len(res.text))
+print('read total byte:'+str(len(res.text)))
 
 srcFile=open('srcData.txt','wb')
 for chunk in res.iter_content(len(res.text)):
@@ -18,42 +18,48 @@ type(nSoup)
 
 elems = nSoup.select('script')
 type(elems)
-print(len(elems))
+print("scripts byte:"+str(len(elems)))
 
 targetList=[]
-targetURL=target1=''
-count_i=0
+target1=[]
+tarURL=[]
+targetURL=''
+count_i=1
 for data in elems:
     if(-1 != data.text.find('var mhurl')):
-        print(count_i)
+        #print(count_i)
         targetList.append(data.text.split(';'))
-    count_i = count_i + 1
+    #count_i = count_i + 1
 
-print(targetList)
+#print(targetList)
 
-for pngs in targetList[0]:
-    if(pngs.find('var mhurl')==0):
-        target1=pngs
+for list_1 in targetList:
+    for pngs in list_1:
+        if(pngs.find('var mhurl')==0):
+            print("contain pngs:"+str(pngs))
+            target1.append(pngs)
 
-print('target1:'+target1)
+for i in target1:
+    print(i)
+    targetURL=i.split('=')[1][1:-1]
+    print(targetURL)
+    part1='http://p0.manhuapan.com/'+targetURL
+    tarURL.append(part1)
 
-targetURL=target1.split('=')[1][1:-1]
-print(targetURL)
-part1='http://p0.manhuapan.com/'+targetURL
-print(part1)
+print(tarURL)
 
-res1 = requests.get(part1)
+for pages in tarURL:
+    res1 = requests.get(pages)
+    try:
+        res1.raise_for_status()
+    except Exception as exc:
+        print('there was a problem:%s'%(exc))
 
-try:
-    res1.raise_for_status()
-except Exception as exc:
-    print('there was a problem:%s'%(exc))
-    
-print(len(res1.text))
+    print('png len:'+str(len(res1.text)))
 
-srcFile1=open('First.png','wb')
-for chunk in res1.iter_content(len(res1.text)):
-    srcFile1.write(chunk)
-srcFile1.close()
+    srcFile1=open(str(count_i)+'.png','wb')
+    for chunk in res1.iter_content(len(res1.text)):
+        srcFile1.write(chunk)
+    srcFile1.close()
+    count_i=count_i+1;
 
-    
